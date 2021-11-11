5743225d0ac3eb8e34df88f70c19ccc7c6bb30f9
unknown <rajputnikita5666@gmail.com>
2021-11-11 17:21:13 +0530

    Added:DB connection establish and load driver

---
+++ src/main/java/com/bridgelabz/jdbc/EmployeePayRoll.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.jdbc;
+
+public class EmployeePayRoll {
+    public static void main(String[] args) {
+    	System.out.println("..WELCOME TO EMPLOYEE PAYROLL JDBC..");
+    	
+    	 EmployeeConfig eConfig = new EmployeeConfig();
+         eConfig.getConfig();
+    }
+}
