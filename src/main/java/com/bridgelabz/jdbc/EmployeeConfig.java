964431b05c635224b5364c59bc1252a05eb376ff
unknown <rajputnikita5666@gmail.com>
2021-11-14 20:43:37 +0530

    Added:retrive data from data base

--- src/main/java/com/bridgelabz/jdbc/EmployeeConfig.java
+++ src/main/java/com/bridgelabz/jdbc/EmployeeConfig.java
@@ -5,20 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class EmployeeConfig {
-
-    public Connection getConfig(){
-        String URL_JD = "jdbc:mysql://localhost:3306/payroll_service";
-        String USER_NAME = "Nikita";
-        String PASSWORD = "Root";
-        Connection connection = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Drivers loaded!!");
-            connection = DriverManager.getConnection(URL_JD,USER_NAME,PASSWORD);
-            System.out.println("connection Established!!");
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
+	public Connection getConfig() {
+		String URL_JD = "jdbc:mysql://localhost:3306/payroll_service";
+		String USER_NAME = "Nikita";
+		String PASSWORD = "Root";
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Drivers loaded!!");
+			connection = DriverManager.getConnection(URL_JD, USER_NAME, PASSWORD);
+			System.out.println("connection Established!!");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
 }
---
+++ src/main/java/com/bridgelabz/jdbc/EmployeeException.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.jdbc;
+
+public class EmployeeException extends RuntimeException{
+
+	public EmployeeException(String message) {
+			super(message);
+		}
+
+}
